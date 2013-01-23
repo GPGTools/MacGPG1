7611e5e0d30e0ba2da548d60b9c7a2b09d5a6564
Mento <Mento@gpgtools.org>
2013-01-23 17:19:19 +0100

    some optimazation

--- build.sh
+++ build.sh
@@ -10,17 +10,20 @@
 ##
 
 ################################################################################
-url="ftp://ftp.gnupg.org/gcrypt/gnupg/";
-version="gnupg-1.4.13";
-fileExt=".tar.gz";
+url="ftp://ftp.gnupg.org/gcrypt/gnupg/"
+version="gnupg-1.4.13"
+fileExt=".tar.gz"
 sigExt=".tar.gz.sig"
-build="`pwd`/build/gnupg";
-prefix_build="`pwd`/build/MacGPG1";
+build="$PWD/build/gnupg"
+prefix_build="$PWD/build/MacGPG1"
 prefix_install="/usr/local/MacGPG1"
-gpgKeysFile="GnuPG Keys.gpg";
+gpgKeysFile="$PWD/GnuPG Keys.gpg"
 ################################################################################
 
-if [ "$1" == "check" ]; then
+source "./Dependencies/GPGTools_Core/newBuildSystem/core.sh"
+
+
+if [[ "$1" == "check" ]]; then
 	cd "$build/$version"
 	make check
 	exit $?
@@ -30,7 +33,7 @@ function iSysrootFlag {
 	sdk_version=$1
 	sdk_versions="$sdk_version 10.8 10.7"
 	xcode_path=$(xcode-select -print-path)
-	if [ "$xcode_path" == "/Applications/Xcode.app/Contents/Developer" ]; then
+	if [[ "$xcode_path" == "/Applications/Xcode.app/Contents/Developer" ]]; then
 		sdks_path="$xcode_path/Platforms/MacOSX.platform/Developer/SDKs"
 	else
 		sdks_path="$xcode_path/SDKs"
@@ -39,7 +42,7 @@ function iSysrootFlag {
 	# Check if the requested version is available
 	for version in $sdk_versions; do
 		sdk_path="$sdks_path/MacOSX${version}.sdk"
-		if [ -d "$sdk_path" ]; then
+		if [[ -d "$sdk_path" ]]; then
 			echo "-isysroot $sdk_path"
 			break
 		fi
@@ -47,9 +50,9 @@ function iSysrootFlag {
 }
 
 ################################################################################
-os_ver=`sw_vers -productVersion|cut -f2 -d'.'`
+os_ver=$(sw_vers -productVersion | cut -f2 -d.)
 #todo: check for ppc gcc and instaleld sdk here instead
-if [ $os_ver -ge 6 ]; then
+if [[ $os_ver -ge 6 ]]; then
     export MACOSX_DEPLOYMENT_TARGET="10.6"
     export CFLAGS="-mmacosx-version-min=10.6 -DUNIX $(iSysrootFlag "10.6") -arch x86_64"
 else
@@ -58,47 +61,36 @@ else
 fi
 ################################################################################
 
-pushd "$1" > /dev/null
 
-[ "`which gpg`" != "" ] && gpg --import "$gpgKeysFile";
-mkdir -p "$build";
-mkdir -p "$target";
-cd "$build";
+mkdir -p "$build"
+cd "$build"
 
-if [ ! -e "$version$fileExt" ]; then
-    curl -O "$url$version$fileExt" && curl -O "$url$version$sigExt"
-    if [ "$?" != "0" ]; then
-        echo "Could not get the sources!";
-        exit 1;
-    fi
+if [[ ! -e "$version$fileExt" ]]; then
+    curl -O "$url$version$fileExt" && curl -O "$url$version$sigExt" ||
+		errExit "Could not get the sources!"
 fi
 
-if [ "`which gpg`" != "" ]; then
-    gpg --verify "$version$sigExt"
-    if [ "$?" != "0" ]; then
-        echo "Could not verify the sources!";
-      exit 1;
-    fi
+
+if command -v gpg >/dev/null 2>&1 ;then
+	gpg --import "$gpgKeysFile"
+    gpg --verify "$version$sigExt" ||
+		errExit "Could not verify the sources!"
 fi
 
-tar -xzf "$version$fileExt";
-cd "$version";
+tar -xzf "$version$fileExt"
+cd "$version"
 ./configure \
   --enable-static=yes \
   --disable-endian-check \
   --disable-dependency-tracking \
   --disable-asm \
   --enable-osx-universal-binaries \
-  --prefix="$prefix_install" && \
-make -j2
-#&& \
-#make check
+  --prefix="$prefix_install" ||
+	errExit "Could not configure the sources!"
 
-if [ "$?" != "0" ]; then
-    echo "Could not compile the sources!";
-    exit 1;
-fi
+make -j2 ||
+	errExit "Could not compile the sources!"
+
 
 make prefix="$prefix_build" install
 
-popd > /dev/null
