2b9c90439f2459763c4be81c5803d115ea44a0f3
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-07-30 12:36:49 +0200

    Update to 1.4.14 which includes a patch to mitigate a side channel attack - http://eprint.iacr.org/2013/448

--- build-script.sh
+++ build-script.sh
@@ -11,7 +11,7 @@
 
 ################################################################################
 url="ftp://ftp.gnupg.org/gcrypt/gnupg/";
-version="gnupg-1.4.13";
+version="gnupg-1.4.14";
 fileExt=".tar.gz";
 sigExt=".tar.gz.sig"
 build="`pwd`/build/gnupg";
